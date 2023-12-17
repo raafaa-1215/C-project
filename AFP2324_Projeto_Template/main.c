45e70c7b96b43c09417017f33f1ff864446efce2
raafaa-1215 <131355317+raafaa-1215@users.noreply.github.com>
2023-12-17 21:07:55 +0000

    Template inicial

---
+++ AFP2324_Projeto_Template/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "input.h"
+
+int main() {
+
+    // TODO: implement menu and call subprograms for each operation
+
+    printf("I'm missing a menu! :( \n");
+
+    return EXIT_SUCCESS;
+}
+

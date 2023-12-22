9f77c6a3e1cbc0a5e4f50f9625af85c85eba78cf
raafaa-1215 <131355317+raafaa-1215@users.noreply.github.com>
2023-12-22 19:49:38 +0000

    inicio dos menus e coleta de comandos

--- main.c
+++ main.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "input.h"
+#include <ctype.h>
+
+void printCommands(char *commandsList) {
+    printf("\n Os comandos disponiveis sao: \n");
+    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("- %s\n", commandsList[i]);
+    }
+    
+    printf("\n\n");
+}
+
+
+
+int getCommand(char *str, char *commandsList, int *size) {
+    int i;
+
+    do
+    {
+        printf("Introduza o comando: \n");
+        gets(str);
+
+        for (i = 0; str != '\0' ; i++)
+        {
+            str[i] = toupper(str[i]);
+        }
+        
+        for (i = 0; i < size; i++)
+        {   
+            if (strcmp(str, commandsList[i]))
+            {
+                break;
+            }
+        }
+
+    } while (1);
+    
+    return i;
+}
 
 int main() {
+    char commandsList[5] = {"LOAD", "CLEAR", "QUIT", "LIST", "FOUNDP"};
+    int size = 5;
+    char response[10];
 
     // TODO: implement menu and call subprograms for each operation
 

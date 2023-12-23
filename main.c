6fb0622e59476c1f78f4de81352e5cacee89c46e
raafaa-1215 <131355317+raafaa-1215@users.noreply.github.com>
2023-12-23 13:11:57 +0000

    menu funcional e coleta da resposta do user

--- main.c
+++ main.c
@@ -1,57 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "input.h"
 #include <ctype.h>
+#include "input.h"
 
-void printCommands(char *commandsList) {
-    printf("\n Os comandos disponiveis sao: \n");
+void printCommands(char (*commandsList)[10]) {
+    printf("\nOs comandos disponiveis sao: \n");
     
     for (int i = 0; i < 5; i++)
     {
-        printf("- %s\n", commandsList[i]);
+        printf("  - %s\n", commandsList[i]);
     }
     
     printf("\n\n");
 }
 
-
-
-int getCommand(char *str, char *commandsList, int *size) {
+int getCommand(char *str, char (*commandsList)[10], int *size) {
     int i;
 
     do
     {
-        printf("Introduza o comando: \n");
+        printf("Introduza o comando ('help' para listar comandos):\n");
         gets(str);
 
-        for (i = 0; str != '\0' ; i++)
+        for (i = 0; str[i] != '\0' ; i++)
         {
-            str[i] = toupper(str[i]);
+            str[i] = toupper((unsigned char)str[i]);
+        }
+
+        if (strcmp(str, "HELP") == 0)
+        {
+            printCommands(commandsList);
+            continue;
         }
         
-        for (i = 0; i < size; i++)
+        for (i = 0; i < *size; i++)
         {   
-            if (strcmp(str, commandsList[i]))
+            if (strcmp(str, commandsList[i]) == 0)
             {
-                break;
+                goto endCheck;
             }
         }
 
+        if (i == *size)
+        {
+            printf("Comando nao encontrado, tente novamente.\n\n");
+        }
     } while (1);
     
+    endCheck:
+
     return i;
 }
 
 int main() {
-    char commandsList[5] = {"LOAD", "CLEAR", "QUIT", "LIST", "FOUNDP"};
-    int size = 5;
-    char response[10];
+    char commandsList[][10] = {"LOAD", "CLEAR", "QUIT", "LIST", "FOUNDP"};
+    int size, commandIndex;
+    char res[10];
+
+    size = sizeof(commandsList) / sizeof(commandsList[0]);
 
     // TODO: implement menu and call subprograms for each operation
 
-    printf("I'm missing a menu! :( \n");
-
-    return EXIT_SUCCESS;
+    commandIndex = getCommand(res, commandsList, &size);
+    
 }
 
